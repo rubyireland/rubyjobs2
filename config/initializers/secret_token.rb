6452514052dd877e9499f508b40bff6b7dbdd943
Gavin Joyce <gavinjoyce@gmail.com>
2013-01-12 12:37:31 -0800

    Initial import.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Rubyjobs2::Application.config.secret_token = '08cbf267b2b42631bcddee511ae6c9f2c49b116b5c528e9059fffadcc4c1ce5e794f5ef911faec6cfff30ccf8e74c23a673624c96d239160c21f2b57d96ea1ce'
