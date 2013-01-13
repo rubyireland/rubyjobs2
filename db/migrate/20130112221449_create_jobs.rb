a26a66cb664f03e237d03b845166a2203354920a
Gavin Joyce <gavinjoyce@gmail.com>
2013-01-12 21:33:56 -0800

    increasing the size of job.title and job.company

--- db/migrate/20130112221449_create_jobs.rb
+++ db/migrate/20130112221449_create_jobs.rb
@@ -2,13 +2,13 @@ class CreateJobs < ActiveRecord::Migration
   def up
     create_table :jobs do |t|
 			
-			t.column :company,						:string, :limit => 50
+			t.column :company,						:string, :limit => 200
 			t.column :url,								:string, :limit => 300
 			t.column :email,							:string, :limit => 200
 			
 			t.column :type_id,						:integer
 			t.column :location_id,				:integer
-			t.column :title,							:string, :limit => 50
+			t.column :title,							:string, :limit => 200
 			t.column :description,				:string, :limit => 8000
 			t.column :how_to_apply,				:string, :limit => 100
 			t.column :key, 								:string
